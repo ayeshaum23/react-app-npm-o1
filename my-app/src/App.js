07f957ed068911d2bef96b494740103af2dddb90
ayesha23 <ayeshaumair56@hotmail.com>
2025-10-25 23:50:43 +0500

    revert header content

---
+++ my-app/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        
+      <main className="main-content"> Content Body !</main>  
+        
+      {/* // HomePage
+      // ContactPage
+      // AboutPage
+      // ServicesPage
+      // BlogPage
+      // CareersPage
+      // SupportPage */}
+
+      <Footer />
+      
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>this is our new para.. editing the index.html</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ my-app/src/component/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className='app-header'>
+      <h1>This is header component.</h1>
+    </div>
+  );
+}
+export default Header;
